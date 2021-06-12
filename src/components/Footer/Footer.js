6c59b3eb4e1e8172593532b3f6744ac1b8113867
pras75299 <singh.prashantking@gmail.com>
2021-06-12 16:39:14 +0530

    complete footer and acomplishments section

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,9 +6,32 @@ import { CompanyContainer, FooterWrapper, LinkColumn, LinkItem, LinkList, LinkTi
 
 const Footer = () => {
   return (
-    <div>
-      Footer
-    </div>
+    <FooterWrapper>
+      <LinkList>
+        <LinkColumn>
+          <LinkTitle>Call</LinkTitle>
+          <LinkItem href="https://github.com/">9818020030</LinkItem>
+        </LinkColumn>
+        <LinkColumn>
+          <LinkTitle>Email</LinkTitle>
+          <LinkItem href="mailto:singh.prashantking@gmail.com">singh.prashantking@gmail.com</LinkItem>
+        </LinkColumn>
+      </LinkList>
+      <SocialIconsContainer>
+        <CompanyContainer>
+          <Slogan>Lorem ipsum lorem in </Slogan>
+        </CompanyContainer>
+        <SocialIcons href="https://github.com/pras75299">
+          <AiFillGithub size="3rem"/>
+        </SocialIcons>
+        <SocialIcons href="https://github.com">
+          <AiFillLinkedin size="3rem"/>
+        </SocialIcons>
+        <SocialIcons href="https://github.com/pras75299">
+          <AiFillInstagram size="3rem"/>
+        </SocialIcons>
+      </SocialIconsContainer>
+    </FooterWrapper>
   );
 };
 
